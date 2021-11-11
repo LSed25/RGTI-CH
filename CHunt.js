171769a261cca778b0067599c3a6d5e3c7c1e124
LSed25 <lucija.sedovsek@gmail.com>
2021-11-11 20:20:41 +0100

    js file

--- CHunt.js
+++ CHunt.js
@@ -1,2 +1,20 @@
 import { Application } from '../../common/engine/Application.js';
 import { WebGL } from '../../common/engine/WebGL.js';
+import { GUI } from '../../lib/dat.gui.module.js';
+
+class App extends Application {
+
+    start() {
+
+    }
+
+    render() {
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector('canvas');
+    const app = new App(canvas);
+    const gui = new GUI();
+});
